7702c1ffff43feb61d7a07d09e8dc389e1509d0c
Caio Patriota <caiopatriotabarros@gmail.com>
2024-02-27 20:54:37 -0300

    update

---
+++ .history/problem_set_2/readability_20240227205317.c
@@ -0,0 +1,52 @@
+#include <cs50.h>
+#include <ctype.h>
+#include <math.h>
+#include <stdio.h>
+#include <string.h>
+
+int main(void)
+{
+    // Prompt the user for some text
+    string text = get_string("Text: ");
+
+    // Count the number of letters, words, and sentences in the text
+    int letters = 0;
+    int words = 1;
+    int sentences = 0;
+
+    for (int i = 0; i < strlen(text); i++)
+    {
+        if (isalpha(text[i]))
+        {
+            letters++;
+        }
+        else if (text[i] == ' ')
+        {
+            words++;
+        }
+        else if (text[i] == '.' || text[i] == '!' || text[i] == '?')
+        {
+            sentences++;
+        }
+    }
+
+    // Compute the Coleman-Liau index
+    float L = (float) letters / (float) words * 100;
+    float S = (float) sentences / (float) words * 100;
+
+    int index = round(0.0588 * L - 0.296 * S - 15.8);
+
+    // Print the grade level
+    if (index < 1)
+    {
+        printf("Before Grade 1\n");
+    }
+    else if (index >= 16)
+    {
+        printf("Grade 16+\n");
+    }
+    else
+    {
+        printf("Grade %i\n", index);
+    }
+}
